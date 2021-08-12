d01884f7058e7a02688db8433f29fdac9259f7a7
Nika <nabakhtevelin@gmail.com>
2021-08-13 00:55:27 +0400

    added bugs to fix later

--- assets/components/scrapIpn.js
+++ assets/components/scrapIpn.js
@@ -7,15 +7,34 @@ export default function scrapIpn(url, accept, accept1, sourceImgUrl) {
     if (!error && response.statusCode == 200) {
       const $ = cheerio.load(html);
 
-      const container = $('.container').html();
-      const title = $('.articletitle').text();
+      const siteHeading = $('.container');
+      // const dataInfo = $('.dateandsource').children('span').first().text();
       const dataInfo = $('.articledate').find('span').text();
-      const text = $('.articlecontent_in').find('p').text();
-      const imgUrl = $('.articleimg').find('img').attr("src");
+      const title = siteHeading.find('h1').text();
+      const text = $('.articlecontent_in').children().text();
+      const imgUrl = $('.articleimg').attr("src");
+
+      console.log(siteHeading.html());
 
 
-      
-      if(accept==="on"){
+      if (accept === "on" && accept1 === "on") {
+        fs.readFile('./assets/data/girchi.json', (err, data) => {
+          if (err) throw err;
+          let newsData = JSON.parse(data);
+          newsData[dataInfo] = {
+            ...newsData[dataInfo],
+            title: title,
+            text: text,
+            link: url,
+            logo: sourceImgUrl,
+            articleDate: dataInfo,
+            imgUrl: imgUrl
+          };
+          newsData = JSON.stringify(newsData)
+          fs.writeFileSync("./assets/data/girchi.json", newsData, (error) => {
+            if (error) console.log(error)
+          })
+        });
         fs.readFile('./assets/data/important.json', (err, data) => {
           if (err) throw err;
           let newsData = JSON.parse(data);
@@ -50,7 +69,77 @@ export default function scrapIpn(url, accept, accept1, sourceImgUrl) {
             if (error) console.log(error)
           })
         });
-      }else{
+      } else if (accept === "on") {
+        fs.readFile('./assets/data/important.json', (err, data) => {
+          if (err) throw err;
+          let newsData = JSON.parse(data);
+          newsData[dataInfo] = {
+            ...newsData[dataInfo],
+            title: title,
+            text: text,
+            link: url,
+            logo: sourceImgUrl,
+            articleDate: dataInfo,
+            imgUrl: imgUrl
+          };
+          newsData = JSON.stringify(newsData)
+          fs.writeFileSync("./assets/data/important.json", newsData, (error) => {
+            if (error) console.log(error)
+          })
+        });
+        fs.readFile('./assets/data/ipn.json', (err, data) => {
+          if (err) throw err;
+          let newsData = JSON.parse(data);
+          newsData[dataInfo] = {
+            ...newsData[dataInfo],
+            title: title,
+            text: text,
+            link: url,
+            logo: sourceImgUrl,
+            articleDate: dataInfo,
+            imgUrl: imgUrl
+          };
+          newsData = JSON.stringify(newsData)
+          fs.writeFileSync("./assets/data/ipn.json", newsData, (error) => {
+            if (error) console.log(error)
+          })
+        });
+      } else if (accept1 === "on") {
+        fs.readFile('./assets/data/girchi.json', (err, data) => {
+          if (err) throw err;
+          let newsData = JSON.parse(data);
+          newsData[dataInfo] = {
+            ...newsData[dataInfo],
+            title: title,
+            text: text,
+            link: url,
+            logo: sourceImgUrl,
+            articleDate: dataInfo,
+            imgUrl: imgUrl
+          };
+          newsData = JSON.stringify(newsData)
+          fs.writeFileSync("./assets/data/girchi.json", newsData, (error) => {
+            if (error) console.log(error)
+          })
+        });
+        fs.readFile('./assets/data/ipn.json', (err, data) => {
+          if (err) throw err;
+          let newsData = JSON.parse(data);
+          newsData[dataInfo] = {
+            ...newsData[dataInfo],
+            title: title,
+            text: text,
+            link: url,
+            logo: sourceImgUrl,
+            articleDate: dataInfo,
+            imgUrl: imgUrl
+          };
+          newsData = JSON.stringify(newsData)
+          fs.writeFileSync("./assets/data/ipn.json", newsData, (error) => {
+            if (error) console.log(error)
+          })
+        });
+      } else {
         fs.readFile('./assets/data/ipn.json', (err, data) => {
           if (err) throw err;
           let newsData = JSON.parse(data);
