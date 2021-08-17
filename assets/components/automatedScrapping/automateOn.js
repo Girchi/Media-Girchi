3e86461f4307545bc648160d61b92498b54904b8
nnabakhteveli <nabakhtevelin@gmail.com>
2021-08-17 22:55:19 +0400

    Separated On.ge's automated scrapping as modules

--- assets/components/automatedScrapping/automateOn.js
+++ assets/components/automatedScrapping/automateOn.js
@@ -1,39 +1,6 @@
-import fs from 'fs';
 import request from 'request';
 import cheerio from 'cheerio';
-
-
-function writeToSource(url, fileName, sourceName) {
-  request(url, (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-      const $ = cheerio.load(html);
-
-      const title = $(".article-title").text();
-      const dataInfo = $(".date").first().text();
-      const text = $(".article-body").text();
-      const imgUrl = `https:${$(".global-figure-image  ").attr("src")}`;
-
-
-      let newsData = JSON.parse(fs.readFileSync(`./assets/data/${fileName}`, 'utf-8'));
-      newsData[dataInfo] = {
-        ...newsData[dataInfo],
-        source: sourceName,
-        title: title,
-        text: text,
-        link: url,
-        logo: "http://gip.ge/wp-content/uploads/2017/10/apple-touch-icon.png",
-        articleDate: dataInfo,
-        imgUrl: imgUrl,
-        important: false,
-        fileName: fileName
-      };
-      newsData = JSON.stringify(newsData)
-      fs.writeFileSync(`./assets/data/${fileName}`, newsData, (error) => {
-        if (error) console.log(error)
-      })
-    }
-  })
-}
+import automatedWriteToSource from '../writingData/automatedWriteToSource.js';
 
 
 export default function automateOn() {
@@ -42,11 +9,10 @@ export default function automateOn() {
       const $ = cheerio.load(html);
       let obj = $('.row').find('section').find('a.overlay-link');
 
-      // console.log(object[1].children[0].data)
       let text = obj[3].children[0].data;
       let url = obj[3].attribs.href;
       for (let i = 0; i < obj.length; i++) {
-        writeToSource(obj[i].attribs.href, "on.json", "On");
+        automatedWriteToSource(obj[i].attribs.href, "on.json", "On", "http://gip.ge/wp-content/uploads/2017/10/apple-touch-icon.png");
       }
     } else {
       console.log("Something failed!");
---
+++ assets/components/writingData/automatedWriteToSource.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import request from 'request';
+import cheerio from 'cheerio';
+
+export default function writeToSource(url, fileName, sourceName, logoUrl) {
+    request(url, (error, response, html) => {
+      if (!error && response.statusCode === 200) {
+        const $ = cheerio.load(html);
+  
+        const title = $(".article-title").text();
+        const dataInfo = $(".date").first().text();
+        const text = $(".article-body").text();
+        const imgUrl = `https:${$(".global-figure-image  ").attr("src")}`;
+  
+  
+        let newsData = JSON.parse(fs.readFileSync(`./assets/data/${fileName}`, 'utf-8'));
+        newsData[dataInfo] = {
+          ...newsData[dataInfo],
+          source: sourceName,
+          title: title,
+          text: text,
+          link: url,
+          logo: logoUrl,
+          articleDate: dataInfo,
+          imgUrl: imgUrl,
+          important: false,
+          fileName: fileName
+        };
+        newsData = JSON.stringify(newsData)
+        fs.writeFileSync(`./assets/data/${fileName}`, newsData, (error) => {
+          if (error) console.log(error)
+        })
+      }
+    })
+  }
