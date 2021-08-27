6e8b2059b9e6c70c6fb45924cf9df173ac840563
nnabakhteveli <nabakhtevelin@gmail.com>
2021-08-27 17:56:24 +0400

    Only logged users have an access to /trusted-guy route

--- assets/components/writingData/checkIfFileIsEmpty.js
+++ assets/components/writingData/checkIfFileIsEmpty.js
@@ -1,14 +1,14 @@
 import fs from 'fs';
 
 export default function checkFile(path, callback) {
-    fs.readFile(path, (err, data) => {
-        if (data.length === 0) {
-          fs.writeFile(path, '{}', 'utf-8', (err) => {
-            if(err) throw err;
-            callback;
-          });
-        } else {
-          callback;
-        }
-    })
+  fs.readFile(path, (err, data) => {
+    if (data.length === 0) {
+      fs.writeFile(path, '{}', 'utf-8', (err) => {
+        if (err) throw err;
+        callback;
+      });
+    } else {
+      callback;
+    }
+  })
 }
